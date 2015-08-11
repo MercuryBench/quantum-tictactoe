cafaabdc2e4f7953f7c370cf066978101920a3ca
= <=>
2015-08-11 10:53:47 +0200

    First steps in setting up the game. Still largely incomplete, will not run

---
+++ qTTT.py
@@ -0,0 +1,139 @@
+import random
+
+class Board:
+	def __init__(self):
+		self.fields = [Field(n) for n in range(10)] # ignore index 0
+	
+	def addPreMark(self, letter, num, pos1, pos2):
+		self.fields[pos1].addPreMark(PreMark(letter, num, pos1, pos2))
+		self.fields[pos2].addPreMark(PreMark(letter, num, pos2, pos1))
+	
+	def addFinMark(self, letter, pos):
+		self.fields[pos].addFinMark(FinMark(letter, pos))
+		
+	def hasWon(self, letter):
+		bo = self.realBoard()
+		return ((bo[7] == le and bo[8] == le and bo[9] == le) or # across the top
+
+		(bo[4] == le and bo[5] == le and bo[6] == le) or # across the middle
+
+		(bo[1] == le and bo[2] == le and bo[3] == le) or # across the bottom
+
+		(bo[7] == le and bo[4] == le and bo[1] == le) or # down the left side
+
+		(bo[8] == le and bo[5] == le and bo[2] == le) or # down the middle
+
+		(bo[9] == le and bo[6] == le and bo[3] == le) or # down the right side
+
+		(bo[7] == le and bo[5] == le and bo[3] == le) or # diagonal
+
+		(bo[9] == le and bo[5] == le and bo[1] == le)) # diagonal
+
+	def realBoard(self): # returns only the "real" board, i.e. measured fields
+		bo = [' '] * 10
+		for f in self.fields:
+			for m in f.contents:
+				if isinstance(m, FinMark):		
+					bo[f.num] = m.letter
+		return bo
+		
+	def printBoard(self):
+		for f in self.fields:
+			print("Field" + str(f.num))
+			print(f.fieldToString())
+			
+	def isSpaceFree(self, move):
+		# Return true if the passed move is free on the passed board.
+		board = self.realBoard()
+		return board[move] == ' '
+
+	def getListOfMoves(self):
+		listOfMoves = []
+		for move in range(1, 10):
+			if isSpaceFree(board, move):
+				listOfMoves.append(move)
+		return listOfMoves
+		
+class Field:
+	def __init__(self, num):
+		self.contents = []
+		self.num = num
+		
+	def addPreMark(self, pmark):
+		if self.num != pmark.pos:
+			print("Error: Wrong Mark position")
+			return
+		self.contents.append(pmark)
+		
+	def addFinMark(self, fmark):
+		if self.num != fmark.pos:
+			print("Error: Wrong Final Mark position")
+			return
+		self.contents.append(fmark)
+	
+	def deletePreMark(self, pmark):
+		if pmark in self.contents:
+			v.remove(pmark)
+		else:
+			print("Error: Pre Mark not in Field")
+			
+	def deletePreMark_(self, letter, num):
+		for m in self.contents:
+			if isinstance(m, PreMark):
+				if m.num == num and m.letter == letter:
+					self.contents.remove(m)
+	
+	def deleteFinMark(self, fmark):
+		if fmark in self.contents:
+			self.contents.remove(fmark)
+		else:
+			print("Error: Final Mark not in Field")
+			
+	def fieldToString(self):
+		for m in self.contents:
+			if isinstance(m, FinMark):
+				return m.letter
+		# so apparently there is no final mark
+		s = ""
+		for m in self.contents:
+			s += (m.letter + str(m.num) + ", ")
+		return s
+			
+class PreMark:
+	def __init__(self, letter, num, pos, otherpos):
+		self.letter = letter
+		self.num = num
+		self.pos = pos
+		self.otherpos = otherpos
+		
+class FinMark:
+	def __init__(self, letter, pos):
+		self.letter = letter
+		self.pos = pos
+		
+b = Board()
+b.addPreMark('X', 1, 2, 4)
+b.printBoard()
+b.addPreMark('O', 2, 2, 5)
+b.printBoard()
+b.addFinMark('X', 2)
+b.printBoard()
+print(b.realBoard())
+
+"""def drawBoard(board):
+		# This function prints out the board that it was passed.
+      # "board" is a list of 10 strings representing the board (ignore index 0)
+		print('   |   |')
+		print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+		print('   |   |')
+		print('-----------')
+		print('   |   |')
+		print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+		print('   |   |')
+		print('-----------')
+		print('   |   |')
+		print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+		print('   |   |')
+		
+def drawField(field):		
+		return"""
